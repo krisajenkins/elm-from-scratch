7d29df0c4ce430484fd33b5fa68c14d04dcbaf97
Kris Jenkins <krisajenkins@gmail.com>
2016-07-11 19:05:50 +0100

    State.

--- src/State.elm
+++ src/State.elm
@@ -8,3 +8,15 @@ init =
     ( {}
     , Cmd.none
     )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Nope ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
